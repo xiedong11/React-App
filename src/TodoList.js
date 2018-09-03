fbe171c1bc68151340be7e3ff00b991ac7539cba
xiedong <zx381010>
2018-09-03 14:52:40 +0800

    点击按钮后添加输入框里的内容到列表项

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,27 +5,21 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            list: [
-                'learn react',
-                'learn react',
-                'learn vue',
-                'learn vue',
-            ]
+            list: [],
+            inputValue: ''
         }
-
-
     }
 
     render() {
         return (
             <div>
                 <div>
-                    <input/>
+                    <input  value={this.state.inputValue} onChange={this.handleInputChange.bind(this)}/>
                     <button onClick={this.handleBthClick.bind(this)}>add</button>
                 </div>
                 < ul>
                     {
-                        this.state.list.map((item,index) => {
+                        this.state.list.map((item, index) => {
                             return <li key={index}>{item}</li>
                         })
                     }
@@ -37,11 +31,19 @@ class App extends Component {
     handleBthClick() {
         //...展开运算符，包含list中的所有元素
         this.setState({
-            list: [...this.state.list, "hello word"]
+            list: [...this.state.list, this.state.inputValue],
+            inputValue:"" //完成输入之后，情况输入框内容
         })
 
     }
 
+
+    handleInputChange(e){
+        // console.log(e.target.value)
+        this.setState({
+            inputValue:e.target.value
+        })
+    }
 }
 
 export default App;
