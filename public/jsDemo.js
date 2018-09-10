14d3f3b54e656855e8a4328348c07728f0a692fd
xiedong <zx381010>
2018-09-10 11:42:23 +0800

    添加部分文件

---
+++ public/jsDemo.js
@@ -0,0 +1,18 @@
+// alert("hello word")
+
+//函数
+functionTest(7) //调用函数
+
+var rename= functionTest;//给函数换名字重新使用
+rename("fsadfdsf");
+
+/**
+ * 定义函数
+ * @param count
+ */
+function functionTest(count) {
+    // for (var i = 1; i < 10; i++) {
+    //     console.log("function test。。。")
+    // }
+    console.log(count)
+}
