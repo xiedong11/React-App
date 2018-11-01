6084403e571d8ad8ab5cd660259bf821f2f99440
xiedong <zx381010>
2018-11-01 16:07:27 +0800

    暂存文件

--- public/jsDemo.js
+++ public/jsDemo.js
@@ -3,7 +3,7 @@
 //函数
 functionTest(7) //调用函数
 
-var rename= functionTest;//给函数换名字重新使用
+var rename = functionTest;//给函数换名字重新使用
 rename("fsadfdsf");
 
 /**
@@ -15,4 +15,17 @@ function functionTest(count) {
     //     console.log("function test。。。")
     // }
     console.log(count)
+}
+
+
+var num=funcationSmartVar(1, 2, 3, 4)
+console.log(num)
+//动态参数
+function funcationSmartVar() {   //参数不写，可支持arguments取默认参数，不指定参数个数
+
+    var z = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        z += arguments[i];
+    }
+    return z;
 }
